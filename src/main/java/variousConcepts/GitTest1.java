97c6dbf670569151e6a3066a6b42c7dd54ad4ddc
forevercharles <charles.njoro200@gmail.com>
2021-06-29 09:56:09 -0500

    3rd commit

---
+++ src/main/java/variousConcepts/GitTest1.java
@@ -0,0 +1,22 @@
+package variousConcepts;
+
+public class GitTest1 {
+
+	public static void main(String[] args) {
+
+		printMe();
+		printMe1();
+	}
+
+	
+	public static void printMe() {
+
+		System.out.println("git test");
+	}
+
+	public static void printMe1() {
+
+		System.out.println("git test 1");
+	}
+
+}
